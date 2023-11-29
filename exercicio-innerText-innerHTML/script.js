da698684b7622ec7fc7d6fbaa216a5cefdeec24a
Leandro L. Lobo <148393517+Leandrollobo@users.noreply.github.com>
2023-11-29 00:31:55 -0300

    Add files via upload
    Crie um projeto com dois arquivos: index.html e script.js. No arquivo 'index' insira a estrutura base HTML e dentro da tag 'body' inclua quatro tags vazias: h1, ul, a, ol. Adicione o atributo id="titulo" à tag h1, o atributo href="https://prozeducacao.com.br" à tag 'a', e o atributo id="lista-ordenada" à tag 'ol'. Na sequência, realize a conexão entre o arquivo HTML e o arquivo JavaScript.
    
    No arquivo script.js capture os quatro elementos criados, e use a propriedade .innerText para adicionar conteúdo textual aos elementos 'h1' e 'a', e a propriedade .innerHTML para adicionar três itens simples na lista não ordenada, e três itens com links para outros sites na lista ordenada.

---
+++ exercicio-innerText-innerHTML/script.js
@@ -0,0 +1,25 @@
+let h1 = document.getElementById("titulo");
+console.log(h1);
+
+let ul = document.querySelector("ul");
+console.log(ul);
+
+let a = document.getElementById("a");
+console.log(a);
+
+let ol = document.getElementById("lista-ordenada");
+console.log(ol);
+
+h1.innerText = "Bem vindo em Javascript";
+
+ul.innerHTML = `
+<li> Item 1 </li>
+<li> Item 2 </li>
+<li> Item 3 </li>
+`;
+
+ol.innerHTML = `
+<li><a href="www.google.com.br">Google</a></li>
+<li><a href="www.facebook.com">Facebook</a></li>
+<li><a href="www.instagram.com">Instagram</a></li>
+`;
